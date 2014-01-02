69a65bf800a9dbd971061120d12d588ff2a0d290
Richard Purk <Richard.Purk@googlemail.com>
2014-01-02 13:11:55 +0100

    Content Anzeige auf Index

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,15 @@
 class IndexController < ApplicationController
   def index
+  	content = Content.where("page = 'Index'")
+
+  	@headline = content.find_by_pageId(1).content
+  	@contentHeadline1 = content.find_by_pageId(2).content
+  	@content1 = content.find_by_pageId(3).content
+  	@contentHeadline2 = content.find_by_pageId(4).content
+  	@content2 = content.find_by_pageId(5).content
+  	@contentHeadline3 = content.find_by_pageId(6).content
+  	@content3 = content.find_by_pageId(7).content
+
+
   end
 end
