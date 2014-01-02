1c3565f427d79efc874c5dbc665770f69a16761f
Richard Purk <Richard.Purk@googlemail.com>
2014-01-02 12:04:46 +0100

    Index Content Seed (rake db:seed)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-pass = Ad123
-admin = User.create email: 'jb449388@fh-muenster.de', password: pass,
-password_confirmation: pass
-admin.add_role :admin
-puts "Admin password is #{pass}"
+#pass = Ad123
+#admin = User.create email: 'jb449388@fh-muenster.de', password: pass,
+#password_confirmation: pass
+#admin.add_role :admin
+#puts "Admin password is #{pass}"
 
 
+Content.create({"content"=>"Hochschulranking", "page"=>"Index", "pageId" =>1})
+Content.create({"content"=>"Willkommen", "page"=>"Index", "pageId" =>2})
+Content.create({"content"=>"Finden sie Schnell und gezielt Ihre Hochschule.", "page"=>"Index", "pageId" =>3})
+Content.create({"content"=>"BlaBlub2", "page"=>"Index", "pageId" =>4})
+Content.create({"content"=>"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.", "page"=>"Index", "pageId" =>5})
+Content.create({"content"=>"BlaBlub2", "page"=>"Index", "pageId" =>6})
+Content.create({"content"=>"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.", "page"=>"Index", "pageId" =>7})
