79d3b17d2ce2ed3ef510c2257648c949af25879f
Richard Purk <Richard.Purk@googlemail.com>
2014-01-09 16:27:43 +0100

    ++ University Functions

--- app/controllers/university_functions.rb
+++ app/controllers/university_functions.rb
@@ -2,15 +2,23 @@ class UniversityFunctions
   
   def self.getRating(id)
     evals = Evaluation.find_all_by_university_id(id)
-    return Evaluation.sum(:evaluation, :conditions=>{:university_id=>id}).to_f / evals.count / 5 * 100
+    return Evaluation.sum(:evaluation, :conditions=>{:university_id=>id}).to_f / evals.count
   end
 
   def self.getRatedList
-
+    #return University.select("*").joins("left outer join evaluations where evaluations.university_id = universities.id").order("evaluations.evaluation desc")
+ 
+   	##SQL Statement
+    #select university_id,sum(evaluation), count(evaluation), avg(evaluation) from evaluations group by university_id;
+    #todo bauen
   end
 
  def self.toFivePointRating(percent)
  	return percent / 100 * 5
  end
+
+ def self.toPercenteRating(rating)
+ 	return rating / 5 * 100
+ end 
   
 end
