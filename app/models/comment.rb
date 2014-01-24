14c9ddad6a03c67336886349f7486c04182ad944
Jonny Burnus <jb449388@fh-muenster.de>
2014-01-24 18:43:22 +0100

    Model Comment fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ActiveRecord::Base
-  belongs_to :section
   belongs_to :user
   belongs_to :university
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,3 @@
 class Section < ActiveRecord::Base
   has_many :evaluations
-  has_many :comments
 end
