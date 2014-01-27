8e5752a8c77c984cf8648fc86601db5648ed9793
Niklas <nb900568@fh-muenster.de>
2014-01-27 17:01:02 +0100

    logout wieder hergerichtet

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ HochschulrankingMs::Application.routes.draw do
   post '/rate' => 'rater#create', :as => 'rate'
   get "universities" => "universities#index"
   
-
   get "search" => "search#quickSearch"
   get "search/quickSearch" => "search#quickSearch"
  
