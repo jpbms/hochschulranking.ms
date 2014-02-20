dec572472a1593150056a9a906f526b821001a07
Niklas <nb900568@fh-muenster.de>
2014-02-20 11:32:13 +0100

    Paperclip Anpassung

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,10 @@ class User < ActiveRecord::Base
     Profile.create(user: self)
   end
 
+  #Paperclip Avatar
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "60x60>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   belongs_to :university 
   belongs_to :subject
   letsrate_rater
