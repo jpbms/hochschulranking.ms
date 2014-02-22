f9499dde142ca7dd085ba9be4432d399678ae729
Jan Wübbels <jw159636@fh-muenster.de>
2014-02-22 12:26:38 +0100

    Map Search with Links and Infos

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -17,10 +17,13 @@ class SearchController < ApplicationController
 	else
 	   @classifieds = Address.all
 	end
-    	@hash = Gmaps4rails.build_markers(@classifieds) do |user, marker|
-	      	marker.lat user.latitude
-	      	marker.lng user.longitude
+    	@hash = Gmaps4rails.build_markers(@classifieds) do |address, marker|
+	      	marker.lat address.latitude
+	      	marker.lng address.longitude
+		marker.infowindow "<a href=/universities/" + address.university.id.to_s + ">" + address.university.name + "</a>" + "</p>" + " " + "<p>" + address.street + "</p>" + address.postalcode 
+		marker.json({ :id => address.id })
 	end
   end
+	
 
 end
--- public/js/mapSearch.js
+++ public/js/mapSearch.js
@@ -1,10 +1,10 @@
-	$(document).ready(function() {
-				  handler = Gmaps.build('Google');
-				  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-				  markers = handler.addMarkers(<%=raw @hash.to_json %>);
-				  handler.bounds.extendWith(markers);
-				  handler.fitMapToBounds();
-				  });
+$(document).ready(function() {
+	  handler = Gmaps.build('Google');
+	  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+	  markers = handler.addMarkers(<%=raw @hash.to_json %>);
+	  handler.bounds.extendWith(markers);
+	  handler.fitMapToBounds();
+  	  });
 
 
 });
