eafb270d7084ff4d6c863386791b0fffbfa3a018
Jonny Burnus <jb449388@fh-muenster.de>
2014-02-22 13:45:21 +0100

    Fixes

--- public/js/madhatters.js
+++ public/js/madhatters.js
@@ -332,10 +332,6 @@ $(document).ready(function() {
 
 
 
-    $(function() {
-        $("#tabs").tabs();
-    });
-
     $(document).ajaxSend(function(e, xhr, options) {
         var token = $("meta[name='csrf-token']").attr("content");
         xhr.setRequestHeader("X-CSRF-Token", token);
