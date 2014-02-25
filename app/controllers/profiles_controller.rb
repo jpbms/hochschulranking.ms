ef5d6d6c86c14191849b3f8b3ad16bc126df1d72
Jonny Burnus <jb449388@fh-muenster.de>
2014-02-25 12:25:46 +0100

    Change Profile Show

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -65,8 +65,8 @@ class ProfilesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_profile
-      #@profile = Profile.find(params[:id])
-      @profile = current_user.profile
+      @profile = Profile.find(params[:id])
+      #@profile = current_user.profile
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
